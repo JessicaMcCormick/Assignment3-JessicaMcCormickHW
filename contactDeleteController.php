4532fe7b5ca82c32cd5097dec3f2de90dfbbff1e
Jessica McCormick <jmccor03@uafs.edu>
2025-10-26 20:48:30 -0500

    moving files over

---
+++ contactDeleteController.php
@@ -0,0 +1,34 @@
+<?php
+    require_once 'model/ContactDAO.php';
+
+    //************************
+    //*  Contoller Template  *
+    //************************
+    showErrors(0);  //1 - Turn on Error Display
+
+    $method=$_SERVER['REQUEST_METHOD'];
+    //* Process HTTP GET Request
+    if($method=='GET'){
+        $contactID = $_GET['contactID'];
+        $contactDAO = new ContactDA();
+        $contactDAO -> deleteContact($contactID);
+        header("Locatoin: contactListController.php");
+        exit;
+
+    }
+    
+    //* Process HTTP POST Request
+    if($method=='POST'){
+        
+
+    }
+   
+
+    function showErrors($debug){
+        if($debug==1){
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        }
+    }
+?>
---
+++ views/contactDelete-view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirm Delete</title>
+</head>
+<body>
+    <h1>Are you sure you want to delete this contact?</h1>
+    
+    <form method = "POST" action="contactDeleteController.php">
+        <button type = "submit">Confirm</button>
+        <a href="contactListController.php">Cancel</a>
+    </form>
+</body>
+</html>
